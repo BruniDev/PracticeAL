3a0ac1cda3684a61e3fc5b274bb095c80bb11552
BruniDev <hyeondxx@gmail.com>
2024-05-29 18:01:44 +0900

    Initial Commit

---
+++ practiceAL_trost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practiceAL_trost
+//
+//  Created by 정현 on 5/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
